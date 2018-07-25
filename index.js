dd012742f0f2f0e5a8c8e50ae843dde5562c3813
Mark Semotiuk <marksem@gmail.com>
2018-07-25 19:32:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+var array = Array()
+for(array, ,){array.pop(`$[musicians] plays $[instruments]`)}
+return array
+}
+
+function johnLennonFacts(facts){
+  
+}
+
+function iLoveTheBeatles(){
+  
+}
