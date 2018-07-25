843d70ed0600ab58bfae940471d1c31b18fc7d8f
Mark Semotiuk <marksem@gmail.com>
2018-07-25 21:50:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 function theBeatlesPlay(musicians, instruments){
 var placeholder = Array()
+var musician = musicians[i]
+var instrument = instruments[i]
 for(var i = 0; i < musicians.length; i++) {placeholder.push(`${musicians} plays ${instruments}`)}
 return placeholder
 }
