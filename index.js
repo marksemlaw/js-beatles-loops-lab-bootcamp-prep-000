9175596c5a5072a2e4639580cd6896a52ddc9ac6
Mark Semotiuk <marksem@gmail.com>
2018-07-25 19:46:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // add solution here
 
 function theBeatlesPlay(musicians, instruments){
-var array = Array()
-for(array, ,){array.pop(`$[musicians] plays $[instruments]`)}
+var placeholder = Array()
+for(i = 0, i < placeholder.length, i++) {array.pop(`$[musicians] plays $[instruments]`)}
 return array
 }
 
